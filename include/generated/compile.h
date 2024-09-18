a80a3736ee454d65682d4df9a07e9664a36bf49a
hubinbin <nuaa_hbb@163.com>
2024-09-18 09:34:02 +0800

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Sep 3 14:23:15 CST 2024"
+#define LINUX_COMPILE_BY "hbb"
+#define LINUX_COMPILE_HOST "hbb"
+#define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
