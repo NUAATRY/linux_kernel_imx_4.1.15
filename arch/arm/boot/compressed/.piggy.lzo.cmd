cc30d38ac44c5e25c2fb799639fb2651519c283c
hubinbin <nuaa_hbb@163.com>
2024-12-16 10:59:30 +0800

    add_alsa

--- arch/arm/boot/compressed/.piggy.lzo.cmd
+++ arch/arm/boot/compressed/.piggy.lzo.cmd
@@ -1,1 +1,1 @@
-cmd_arch/arm/boot/compressed/piggy.lzo := (cat arch/arm/boot/compressed/../Image | lzop -9 && printf \\000\\320\\233\\000) > arch/arm/boot/compressed/piggy.lzo || (rm -f arch/arm/boot/compressed/piggy.lzo ; false)
+cmd_arch/arm/boot/compressed/piggy.lzo := (cat arch/arm/boot/compressed/../Image | lzop -9 && printf \\000\\300\\234\\000) > arch/arm/boot/compressed/piggy.lzo || (rm -f arch/arm/boot/compressed/piggy.lzo ; false)
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 4 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Fri Nov 22 15:17:31 CST 2024"
+#define UTS_VERSION "#4 SMP PREEMPT Mon Dec 16 10:27:55 CST 2024"
 #define LINUX_COMPILE_BY "hbb"
 #define LINUX_COMPILE_HOST "hbb"
 #define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
