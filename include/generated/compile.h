a60f2bfb5077ccd26281adab3eaec8d62112cc2f
hubinbin <nuaa_hbb@163.com>
2024-12-21 18:01:37 +0800

    add_iio

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 6 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Mon Dec 16 10:27:55 CST 2024"
+#define UTS_VERSION "#6 SMP PREEMPT Fri Dec 20 15:41:58 CST 2024"
 #define LINUX_COMPILE_BY "hbb"
 #define LINUX_COMPILE_HOST "hbb"
 #define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
