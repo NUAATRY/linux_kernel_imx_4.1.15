51defa1dc86ccf346343922d2b081e77f1f53704
hubinbin <nuaa_hbb@163.com>
2025-01-04 12:35:34 +0800

    add_ch341_uart2

---
+++ drivers/usb/serial/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/usb/serial/built-in.o :=  arm-linux-gnueabihf-ld -EL    -r -o drivers/usb/serial/built-in.o drivers/usb/serial/usbserial.o drivers/usb/serial/ch341.o 
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 12 */
+/* This file is auto generated, version 13 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#12 SMP PREEMPT Fri Dec 27 10:43:53 CST 2024"
+#define UTS_VERSION "#13 SMP PREEMPT Sat Jan 4 10:15:55 CST 2025"
 #define LINUX_COMPILE_BY "hbb"
 #define LINUX_COMPILE_HOST "hbb"
 #define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
